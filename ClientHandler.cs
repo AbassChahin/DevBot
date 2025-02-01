5c56ff1356ee2613249c8f309d25c7b85c25a464
Bo <143742164+AbassChahin@users.noreply.github.com>
2025-02-01 02:37:47 -0500

    Third Commit
    Moved discord client to its own class and added Server Handler

---
+++ ClientHandler.cs
@@ -0,0 +1,66 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace DevBot
+{
+    public class ClientHandler
+    {
+        private static readonly Lazy<ClientHandler> clientHandlerLazy =
+        new Lazy<ClientHandler>(() => new ClientHandler(), LazyThreadSafetyMode.ExecutionAndPublication);
+
+        private static DiscordSocketClient discClient;
+        public static DiscordSocketClient publicDiscClient;
+
+        private ClientHandler() { }
+
+        public static ClientHandler GetInstance() {
+            return clientHandlerLazy.Value;
+        }
+
+        public async Task InitDiscordClient()
+        {
+            if (discClient == null) {
+                await PrepDiscordClient();
+            }
+        }
+
+        private async Task PrepDiscordClient()
+        {
+            discClient = new DiscordSocketClient();
+            discClient.Log += ClientLog;
+            discClient.Ready += ClientReady;
+            discClient.SlashCommandExecuted += SlashCommandHandler;
+            string discordToken = Program.GetEnv("BOT_TOKEN");
+            await discClient.LoginAsync(TokenType.Bot, discordToken);
+            await discClient.StartAsync();
+            publicDiscClient = discClient;
+            await Task.Delay(-1);
+        }
+
+        private static Task ClientLog(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private static async Task ClientReady()
+        {
+            await SetupCommands();
+            Console.WriteLine("BoDevBot Online!");
+        }
+
+        private static async Task SlashCommandHandler(SocketSlashCommand command)
+        {
+            if (command.Data.Name.Equals("hello"))
+            {
+                await command.RespondAsync("Hello!");
+            }
+        }
+
+        private static async Task SetupCommands()
+        {
+            ServerHandler serverHandler = new ServerHandler();
+            await serverHandler.SetupCommands();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,52 +1,18 @@
-﻿using Discord;
-using Discord.Net;
-using Discord.WebSocket;
-using Newtonsoft.Json;
+﻿using Discord.WebSocket;
 using DotNetEnv;
-using System.Dynamic;
+using DevBot;
 
 public class Program {
-    private static DiscordSocketClient _client;
-    private static ulong guildID = (ulong) Convert.ToInt64(GetEnv("GUILD_ID"));
-    
     public static async Task Main()
     {
         Env.Load();
-
-        _client = new DiscordSocketClient();
-        _client.Log += Log;
-        _client.Ready += ClientReady;
-
-        string discordToken = GetEnv("BOT_TOKEN");
-        await _client.LoginAsync(TokenType.Bot, discordToken);
-        await _client.StartAsync();
-
-        await Task.Delay(-1);
+        ClientHandler clientHandler = ClientHandler.GetInstance();
+        await clientHandler.InitDiscordClient();
     }
 
-    private static Task Log(LogMessage msg)
-    {
-        Console.WriteLine(msg.ToString());
-        return Task.CompletedTask;
-    }
 
-    private static async Task ClientReady()
-    {
-        SocketGuild guild = _client.GetGuild(guildID);
-        SlashCommandBuilder guildCommand = new SlashCommandBuilder();
-        guildCommand.WithName("testing");
-        guildCommand.WithDescription("testing");
 
-        try
-        {
-            await guild.CreateApplicationCommandAsync(guildCommand.Build());
-        } catch(HttpException exception) {
-            string errorJson = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
-            Console.WriteLine(errorJson);
-        }
-    }
-
-    private static string GetEnv(string envName)
+    public static string GetEnv(string envName)
     {
         return Environment.GetEnvironmentVariable(envName);
     }
